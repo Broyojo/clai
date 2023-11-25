09f797662e74733d287729590718afec88262c36
Broyojo <dha@xoba.com>
2023-11-25 12:06:49 -0500

    simplified cli structure

--- main.py
+++ main.py
@@ -8,75 +8,136 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     
-    get_global_args(parser)
-
-    tasks_subparser = parser.add_subparsers(title="tasks", dest="task")
-
-    get_transcribe_args(tasks_subparser)
-
-    return parser.parse_args()
-
-def get_global_args(parser):
-    # Model
+    #################### Misc ####################
     
     parser.add_argument(
-        "--model", 
-        type=str, 
-        required=False,
-        help="Name or path of model to use"
+        "--version",
+        action="version",
+        version="%(prog)s 0.1.0",
     )
     
     parser.add_argument(
+        "--verbose",
+        action="store_true",
+        required=False,
+        help="Verbose mode",
+    )
+    
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        required=False,
+        help="Debug mode",
+    )
+    
+    #################### Global Arguments ####################
+    
+    global_args = parser.add_argument_group("Global Arguments")
+    
+    global_args.add_argument(
+        "--model",
+        type=str,
+        required=False,
+        help="Name or path of model to use",
+    )
+    
+    global_args.add_argument(
         "--low_cpu_mem_usage",
         action="store_true",
         required=False,
         help="Use less CPU memory",
     )
     
-    parser.add_argument(
-        "--gpu",
-        action="store_true",
-        required=False,
-        help="Use GPU",
-    )
-    
-    parser.add_argument(
+    global_args.add_argument(
         "--cpu",
         action="store_true",
         required=False,
         help="Use CPU",
     )
     
-def get_transcribe_args(tasks_subparser):
-    transcribe_parser = tasks_subparser.add_parser(
-        name="transcribe",
-        description="Transcribe audio to text",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    # General
-    
-    transcribe_parser.add_argument(
-        "input",
-        type=str,
-        help="Input audio path",
-    )
-    
-    transcribe_parser.add_argument(
-        "output",
-        type=str,
-        help="Output text path",
-    )
-        
-    transcribe_parser.add_argument(
-        "--chunk_batch_size",
+    global_args.add_argument(
+        "--gpu",
+        action="store_true",
         required=False,
+        help="Use GPU",
+    )
+    
+    global_args.add_argument(
+        "--batch_size",
         type=int,
+        required=False,
+        default=1,
+        help="Batch size",
+    )
+    
+    global_args.add_argument(
+        "--compute_type",
+        type=str,
+        required=False,
+        default="float32",
+        choices=["float32", "float16", "float4", "int8"],
+        help="Compute type",
+    )
+    
+    global_args.add_argument(
+        "--use_better_transformer",
+        action="store_true",
+        required=False,
+        help="Use Better Transformer",
+    )
+    
+    global_args.add_argument(
+        "--use_flash_attention",
+        action="store_true",
+        required=False,
+        help="Use Flash Attention",
+    )
+    
+    global_args.add_argument(
+        "--use_flash_attention_2",
+        action="store_true",
+        required=False,
+        help="Use Flash Attention 2",
+    )
+    
+    global_args.add_argument(
+        "--compile",
+        action="store_true",
+        required=False,
+        help="Compile model",
+    )
+    
+    #################### Tasks ####################
+    
+    tasks = parser.add_mutually_exclusive_group()
+    
+    tasks.add_argument(
+        "--transcribe",
+        action="store_true",
+        required=False,
+        help="Perform transcription"
+    )
+    
+    #################### Transcribe Task ####################
+    
+    transcribe_args = parser.add_argument_group("Transcribe Task")
+    
+    transcribe_args.add_argument(
+        "--input",
+        type=str,
+        required=False,
+        help="Input audio path/directory",
+    )
+    
+    transcribe_args.add_argument(
+        "--chunk_batch_size",
+        type=int,
+        required=False,
         default=24,
         help="Size of parallel audio chunk batches you want to compute",
     )
     
-    transcribe_parser.add_argument(
+    transcribe_args.add_argument(
         "--chunk_length",
         required=False,
         type=float,
@@ -84,7 +145,7 @@ def get_transcribe_args(tasks_subparser):
         help="Length of each chunk in seconds",
     )
     
-    transcribe_parser.add_argument(
+    transcribe_args.add_argument(
         "--timestamps",
         required=False,
         type=str,
@@ -93,70 +154,64 @@ def get_transcribe_args(tasks_subparser):
         help="What kind of timestamps to use",
     )
     
-    transcribe_parser.add_argument(
+    transcribe_args.add_argument(
         "--language",
         required=False,
         type=str,
         help="Language of the audio",
     )
     
-    transcribe_parser.add_argument(
+    transcribe_args.add_argument(
         "--translate",
         required=False,
         type=str,
         help="Language to translate the audio to",
     )
     
-    # Diarization
-    
-    diarize_group = transcribe_parser.add_argument_group("Diarization Options")
-
-    diarize_group.add_argument(
+    transcribe_args.add_argument(
         "--diarize",
         action="store_true",
         required=False,
         help="Diarize the audio",
     )
-
-    diarize_group.add_argument(
+    
+    transcribe_args.add_argument(
         "--num_speakers",
         type=int,
         required=False,
         help="Number of speakers to diarize",
     )
     
-    diarize_group.add_argument(
+    transcribe_args.add_argument(
         "--min_speakers",
         type=int,
         required=False,
         help="Minimum number of speakers to diarize",
     )
     
-    diarize_group.add_argument(
+    transcribe_args.add_argument(
         "--max_speakers",
         type=int,
         required=False,
         help="Maximum number of speakers to diarize",
     )
     
-    # VAD
-    
-    vad_group = transcribe_parser.add_argument_group("VAD Options")
-    
-    vad_group.add_argument(
+    transcribe_args.add_argument(
         "--vad",
         action="store_true",
         required=False,
         help="Perform voice activity detection",
     )
     
-    vad_group.add_argument(
+    transcribe_args.add_argument(
         "--no_speech_threshold",
         type=float,
         required=False,
         default=0.6,
         help="Threshold for no speech",
     )
+    
+    return parser.parse_args()
 
 def main(args):
     print(args)
